dff0b178d63cd510310bb05c982dd9acd2753597
Eriq606 <erohweltall@gmail.com>
2025-02-10 04:42:57 +0300

    [refacto] refactored the initialization wizard

--- genesis-intellij/src/main/java/org/labs/genesis/wizards/InitializationWizardStep.java
+++ genesis-intellij/src/main/java/org/labs/genesis/wizards/InitializationWizardStep.java
@@ -9,17 +9,38 @@ import org.labs.genesis.config.langage.Project;
 import org.labs.genesis.forms.InitializationForm;
 
 import javax.swing.*;
+import java.util.HashMap;
 import java.util.Map;
 
 public class InitializationWizardStep extends ModuleWizardStep {
     public final SpecificConfigurationWizardStep specificConfigurationWizardStep;
     private final InitializationForm newProjectPanel;
     private final ProjectGenerationContext projectGenerationContext;
+    // added as attributes to avoid repetition
+    private final String projectName;
+    private final String groupId;
+    private final String location;
+    private final Language language;
+    private final String languageVersion;
+    private final String frameworkVersion;
+    private final Framework framework;
+    private final Project buildTool;
+    private final Framework projectType;
 
     public InitializationWizardStep(ProjectGenerationContext projectGenerationContext, SpecificConfigurationWizardStep specificConfigurationWizardStep) {
         newProjectPanel = new InitializationForm();
         this.projectGenerationContext = projectGenerationContext;
         this.specificConfigurationWizardStep = specificConfigurationWizardStep;
+        // initialization of the added attributes
+        this.projectName = newProjectPanel.getProjectNameField().getText().trim();
+        this.groupId = newProjectPanel.getGroupIdField().getText().trim();
+        this.location = newProjectPanel.getLocationField().getText().trim();
+        this.language = (Language) newProjectPanel.getLanguageOptions().getSelectedItem();
+        this.languageVersion = (String) newProjectPanel.getLanguageVersionOptions().getSelectedItem();
+        this.frameworkVersion = (String) newProjectPanel.getFrameworkVersionOptions().getSelectedItem();
+        this.framework = (Framework) newProjectPanel.getFrameworkOptions().getSelectedItem();
+        this.buildTool = (Project) newProjectPanel.getBuildToolOptions().getSelectedItem();
+        this.projectType = (Framework) newProjectPanel.getFrameworkOptions().getSelectedItem();
     }
 
     @Override
@@ -29,16 +50,6 @@ public class InitializationWizardStep extends ModuleWizardStep {
 
     @Override
     public void updateDataModel() {
-        // Récupérer les valeurs depuis le formulaire
-        String projectName = newProjectPanel.getProjectNameField().getText().trim();
-        String groupId = newProjectPanel.getGroupIdField().getText().trim();
-        String location = newProjectPanel.getLocationField().getText().trim();
-        Language language = (Language) newProjectPanel.getLanguageOptions().getSelectedItem();
-        String languageVersion = (String) newProjectPanel.getLanguageVersionOptions().getSelectedItem();
-        String frameworkVersion = (String) newProjectPanel.getFrameworkVersionOptions().getSelectedItem();
-        Framework framework = (Framework) newProjectPanel.getFrameworkOptions().getSelectedItem();
-        Project buildTool = (Project) newProjectPanel.getBuildToolOptions().getSelectedItem();
-
         assert languageVersion != null;
         assert frameworkVersion != null;
         Map<String, Object> languageConfiguration = Map.of(
@@ -60,20 +71,26 @@ public class InitializationWizardStep extends ModuleWizardStep {
 
     @Override
     public boolean validate() throws ConfigurationException {
-        // Retrieve selected values
-        String projectName = newProjectPanel.getProjectNameField().getText().trim();
-        String groupId = newProjectPanel.getGroupIdField().getText().trim();
-        String location = newProjectPanel.getLocationField().getText().trim();
-        Language language = (Language) newProjectPanel.getLanguageOptions().getSelectedItem();
-        String languageVersion = (String) newProjectPanel.getLanguageVersionOptions().getSelectedItem();
-        String frameworkVersion = (String) newProjectPanel.getFrameworkVersionOptions().getSelectedItem();
-        String framework = (String) newProjectPanel.getCoreFrameworkOptions().getSelectedItem();
-        Framework projectType = (Framework) newProjectPanel.getFrameworkOptions().getSelectedItem();
-        Project buildTool = (Project) newProjectPanel.getBuildToolOptions().getSelectedItem();
-
-        // Validate the project name
-        if (projectName.isEmpty()) {
-            throw new ConfigurationException("The project name cannot be empty.");
+        // declared the validation steps as a map to avoid hardcoded if's
+        HashMap<Boolean, ConfigurationException> validationMap=new HashMap<>(){{
+            put(projectName.isEmpty(), new ConfigurationException("The project name cannot be empty."));
+//                put(groupId.isEmpty() && projectType.getWithGroupId(), new ConfigurationException("The group id cannot be empty."));
+            put(!projectName.matches("^[a-zA-Z0-9_]+$"), new ConfigurationException("""
+                        The project name must be a single word containing only letters, numbers, and underscores.
+                        No special characters or spaces are allowed.
+                    """));
+            put(location.isEmpty(), new ConfigurationException("The location path cannot be empty."));
+            put(language == null, new ConfigurationException("Please select a language."));
+            put(languageVersion == null, new ConfigurationException("Please select a language version."));
+            put(frameworkVersion == null, new ConfigurationException("Please select a framework version."));
+            put(framework == null, new ConfigurationException("Please select a framework."));
+            put(projectType == null, new ConfigurationException("Please select a project type."));
+            put(buildTool == null, new ConfigurationException("Please select a build tool."));
+        }};
+        for(Map.Entry<Boolean, ConfigurationException> e:validationMap.entrySet()){
+            if(e.getKey()){
+                throw e.getValue();
+            }
         }
 
         // Validate the group id
@@ -82,51 +99,6 @@ public class InitializationWizardStep extends ModuleWizardStep {
             if (projectType.getWithGroupId()) {
                 throw new ConfigurationException("The group id cannot be empty.");
             }
-        }
-
-        if (!projectName.matches("^[a-zA-Z0-9_]+$")) {
-            throw new ConfigurationException("""
-                        The project name must be a single word containing only letters, numbers, and underscores.
-                        No special characters or spaces are allowed.
-                    """);
-        }
-
-        // Check that the location path is not empty
-        if (location.isEmpty()) {
-            throw new ConfigurationException("The location path cannot be empty.");
-        }
-
-        // Check that a language is selected
-        if (language == null) {
-            throw new ConfigurationException("Please select a language.");
-        }
-
-        // Check that a language version is selected
-//        if (languageVersion == null || languageVersion.equals("Not applicable")) {
-//            throw new ConfigurationException("Please select a language version.");
-//        }
-        if (languageVersion == null) {
-            throw new ConfigurationException("Please select a language version.");
-        }
-
-        // Check that a framework version is selected
-        if (frameworkVersion == null) {
-            throw new ConfigurationException("Please select a framework version.");
-        }
-
-        // Check that a framework is selected
-        if (framework == null) {
-            throw new ConfigurationException("Please select a framework.");
-        }
-
-        // Check that a project type is selected
-        if (projectType == null) {
-            throw new ConfigurationException("Please select a project type.");
-        }
-
-        // Check that a build tool is selected
-        if (buildTool == null) {
-            throw new ConfigurationException("Please select a build tool.");
         }
 
         // If all validations pass, return true
