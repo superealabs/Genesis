0d48aeb9de0336f06f80146dc42405150a1c352a
tahianarak <169079110+tahianarak@users.noreply.github.com>
2025-07-30 15:25:06 +0300

    getting yaml files tested and some extra file for generating components initialized

--- genesis-core/src/main/java/org/labs/genesis/frontend/generator/IFrontendGenerator.java
+++ genesis-core/src/main/java/org/labs/genesis/frontend/generator/IFrontendGenerator.java
@@ -4,7 +4,7 @@ import org.labs.genesis.connexion.model.TableMetadata;
 import org.labs.genesis.frontend.FrontendLanguage;
 
 public interface IFrontendGenerator {
-    public  String generateComponent(FrontendLanguage language, TableMetadata tableMetadata);
-    public  String generateService(FrontendLanguage language, TableMetadata tableMetadata);
-    public  String generateModel(FrontendLanguage language, TableMetadata tableMetadata);
+    public  String generateComponent(FrontendLanguage language, FrontendFramework frontendFramework,TableMetadata tableMetadata)throws Exception;
+    public  String generateService(FrontendLanguage language,FrontendFramework frontendFramework, TableMetadata tableMetadata)throws Exception;
+    public  String generateModel(FrontendLanguage language,FrontendFramework frontendFramework, TableMetadata tableMetadata)throws Exception;
 }
