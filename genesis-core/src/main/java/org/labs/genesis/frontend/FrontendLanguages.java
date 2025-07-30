f9197de53e17d202500c8c6ecdf8f11ae26ff2fb
Chan Alex <jacqueschanalex@gmail.com>
2025-07-30 09:53:55 +0300

    file to parse the frontend language file

--- genesis-core/src/main/java/org/labs/genesis/frontend/FrontendLanguages.java
+++ genesis-core/src/main/java/org/labs/genesis/frontend/FrontendLanguages.java
@@ -1,4 +1,19 @@
 package org.labs.genesis.frontend;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
 public class FrontendLanguages {
+    private int id;
+    private String name;
+    private Map<String, String> types;
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
