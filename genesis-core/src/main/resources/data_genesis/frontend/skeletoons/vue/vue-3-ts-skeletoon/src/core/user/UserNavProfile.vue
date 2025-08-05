84f9fdeaf507d81766ade18086e64548194eb40f
Chan Alex <jacqueschanalex@gmail.com>
2025-08-05 22:24:45 +0300

    [add] - add implementation of services and major update comming with it

--- genesis-core/src/main/resources/data_genesis/frontend/skeletoons/vue/vue-3-ts-skeletoon/src/core/user/UserNavProfile.vue
+++ genesis-core/src/main/resources/data_genesis/frontend/skeletoons/vue/vue-3-ts-skeletoon/src/core/user/UserNavProfile.vue
@@ -18,4 +18,29 @@
     </div>
   </div>
 </template>
-<script src="./UserNavProfileComponent.ts" lang="ts"></script>
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import GenesisButton from "../button/GenesisButton.vue";
+
+export default defineComponent({
+  name: "UserNavProfile",
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+  },
+  components: { GenesisButton },
+  setup(props) {
+    const initials = computed(() => {
+      if (!props.name) return "";
+      return props.name
+          .split(" ")
+          .map((n) => n[0])
+          .join("")
+          .toUpperCase();
+    });
+    return { initials };
+  },
+});
+</script>
--- genesis-core/src/main/resources/data_genesis/frontend/skeletoons/vue/vue-3-ts-skeletoon/src/models/api/PageResponseModel.ts
+++ genesis-core/src/main/resources/data_genesis/frontend/skeletoons/vue/vue-3-ts-skeletoon/src/models/api/PageResponseModel.ts
@@ -1,30 +1,11 @@
 export interface IPageResponse<T> {
   content: Array<T>;
-  page: PageData;
+  page: IPageModel;
 }
 
-export class PageData {
-  size: number;
-  number: number;
-  totalElements: number;
-  totalPages: number;
-
-  constructor();
-  constructor(
-    size: number,
-    number: number,
-    totalElements: number,
-    totalPages: number
-  );
-  constructor(
-    size?: number,
-    number?: number,
-    totalElements?: number,
-    totalPages?: number
-  ) {
-    this.size = size ?? 10;
-    this.number = number ?? 0;
-    this.totalElements = totalElements ?? 1;
-    this.totalPages = totalPages ?? 1;
-  }
+export interface IPageModel {
+  size: number | undefined;
+  number: number | undefined;
+  totalElements: number | undefined;
+  totalPages: number | undefined;
 }
