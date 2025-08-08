45df2992e781f3b1ee4886ba656fa5e8322fb96f
voarisoamarinah <arisoamarinah@gmail.com>
2025-08-08 16:57:38 +0300

    [Core][Add] View generation methods to GenesisGenerator
    Introduces default methods for generating views and main layout in the GenesisGenerator interface, supporting FrameworkMVC, ViewsTemplate, and ViewsTemplateEngine. These methods return null by default and extend the interface for future view-related functionality.

--- genesis-core/src/main/java/org/labs/genesis/config/langage/generator/framework/GenesisGenerator.java
+++ genesis-core/src/main/java/org/labs/genesis/config/langage/generator/framework/GenesisGenerator.java
@@ -1,7 +1,6 @@
 package org.labs.genesis.config.langage.generator.framework;
 
-import org.labs.genesis.config.langage.Framework;
-import org.labs.genesis.config.langage.Language;
+import org.labs.genesis.config.langage.*;
 import org.labs.genesis.connexion.model.TableMetadata;
 
 import java.util.Map;
@@ -14,4 +13,29 @@ public interface GenesisGenerator {
     String generateService(Framework framework, Map<String, Object> frameworkOptions, Language language, TableMetadata tableMetadata, String destinationFolder, String projectName, String groupLink, boolean generateComponentOnly) throws Exception;
 
     String generateController(Framework framework, Map<String, Object> frameworkOptions, Language language, TableMetadata tableMetadata, String destinationFolder, String projectName, String groupLink, boolean generateComponentOnly) throws Exception;
+
+    default  String generateViews(FrameworkMVC framework,
+                                  Map<String, Object> frameworkOptions,
+                                  Language language,
+                                  ViewsTemplate viewsTemplate,
+                                  ViewsTemplateEngine viewsTemplateEngine,
+                                  TableMetadata tableMetadata,
+                                  String destinationFolder,
+                                  String projectName,
+                                  String groupLink) throws Exception {
+        return null;
+    }
+
+    default  String generateViewMainLayout(FrameworkMVC framework,
+                                           Map<String, Object> frameworkOptions,
+                                           Language language,
+                                           ViewsTemplate viewsTemplate,
+                                           ViewsTemplateEngine viewsTemplateEngine,
+                                           TableMetadata[] tableMetadata,
+                                           String destinationFolder,
+                                           String projectName,
+                                           String groupLink) throws Exception {
+        return null;
+    }
+
 }
