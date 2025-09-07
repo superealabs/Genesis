5d4981f333592abce1bf0d9dc37d7384839f9963
Chan Alex <jacqueschanalex@gmail.com>
2025-09-07 11:40:26 +0300

    [add] - show frontend confuguration wizardstep and init main listeners

--- genesis-intellij/src/main/java/org/labs/genesis/forms/FrontendConfigurationForm.java
+++ genesis-intellij/src/main/java/org/labs/genesis/forms/FrontendConfigurationForm.java
@@ -1,8 +1,11 @@
 package org.labs.genesis.forms;
 
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.ui.components.JBList;
 import lombok.Getter;
+import org.labs.genesis.listener.ColorPicker;
 
 import javax.swing.*;
 import java.awt.*;
@@ -23,19 +26,33 @@ public class FrontendConfigurationForm {
     private JTextField secondaryColorField;
     private JLabel secondaryColorLabel;
     private JButton secondaryColorPickerButton;
-    private JTextField logoLinkField;
     private TextFieldWithBrowseButton logoFileField;
     private JTextArea cssTextArea;
     private TextFieldWithBrowseButton faviconFileField;
+    private JTextField logoLinkField;
     private JTextField faviconLinkField;
 
     public FrontendConfigurationForm() {
-        primaryColorPickerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                JColorChooser colorChooser = new JColorChooser();
-                Color color = JColorChooser.showDialog(null, "Pick a color", Color.BLACK);
-            }
-        });
+        primaryColorPickerButton.addActionListener(new ColorPicker(primaryColorField ,Color.decode("#537cc2")));
+        secondaryColorPickerButton.addActionListener(new ColorPicker(secondaryColorField ,new Color(0x53,0x7c,0xc2,0x26)));
+
+        FileChooserDescriptor faviconChooserDescriptor = FileChooserDescriptorFactory.createSingleFileDescriptor()
+                .withTitle("Select favicon File")
+                .withDescription("Choose a .ico file to load into the editor")
+                .withFileFilter(file -> {
+                    String extension = file.getExtension();
+                    return extension.equals("ico") || extension.equals(".ico");
+                });
+
+        FileChooserDescriptor logoChooserDescriptor = FileChooserDescriptorFactory.createSingleFileDescriptor()
+                .withTitle("Select logo File")
+                .withDescription("Choose image or icon file to load into the editor")
+                .withFileFilter(file -> {
+                    String extension = file.getExtension();
+                    return extension.equals("png") || extension.equals("jpeg") || extension.equals("jpg") || extension.equals("ico");
+                });
+
+        logoFileField.addBrowseFolderListener(null, logoChooserDescriptor);
+        faviconFileField.addBrowseFolderListener(null, faviconChooserDescriptor);
     }
 }
