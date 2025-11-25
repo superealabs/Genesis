b33911a83a1ebfaf62932bf2c3de7218568cdf19
Chan Alex <jacqueschanalex@gmail.com>
2025-11-25 08:49:52 +0300

    init sync in the first form

--- genesis-intellij/src/main/java/org/labs/genesis/forms/FirstForm.java
+++ genesis-intellij/src/main/java/org/labs/genesis/forms/FirstForm.java
@@ -7,16 +7,26 @@ import javax.swing.*;
 @Getter
 public class FirstForm {
     private JPanel mainPanel;
-    private JRadioButton createProject ;
     private JRadioButton addRuleToCode ;
-    private ButtonGroup buttonGroup;
+    private JRadioButton syncProject;
+    private JRadioButton generateNewProject;
+    private JLabel generationLabel;
+    private JLabel rulesLabel;
+    private final ButtonGroup buttonGroup;
 
     public FirstForm() {
         buttonGroup = new ButtonGroup();
-        buttonGroup.add(createProject);
+        buttonGroup.add(generateNewProject);
         buttonGroup.add(addRuleToCode);
+        buttonGroup.add(syncProject);
+        generateNewProject.setSelected(true);
     }
+    public boolean generateNewProjectSelected() {return generateNewProject.isSelected();}
     public boolean ruleTodCodeSelected() {
         return addRuleToCode.isSelected();
     }
+    public boolean syncSelected() {
+        return syncProject.isSelected();
+    }
+
 }
