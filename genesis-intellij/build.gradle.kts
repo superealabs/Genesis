83cbe7623ba2c1dd01abd051508c646093948674
WesleyRS-creator <nyantemaranaivosolohery@gmail.com>
2026-08-08 21:14:23 +0300

    fixing config for intelliJ 2026.1.3

--- genesis-intellij/build.gradle.kts
+++ genesis-intellij/build.gradle.kts
@@ -2,46 +2,49 @@ val projectVersion = rootProject.version
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.24"
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij.platform") version "2.16.0"
 }
 
-val targetIde = "IU"
-val platformVersion = "2025.3"
-val artifactSuffix = "intellij"
-
-intellij {
-    // Dynamiquement injecté selon le build (ou l'IDE actif)
-    version.set(platformVersion)
-    type.set(targetIde)
-    plugins.set(listOf(/* Plugin Dependencies */))
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
     implementation(project(":genesis-core"))
     implementation("com.fifesoft:rsyntaxtextarea:3.3.3")
+
+    intellijPlatform {
+        intellijIdeaUltimate("2026.1.3")
+        testBundledModule("com.intellij.modules.ultimate")
+    }
 }
 
-tasks {
-    patchPluginXml {
-        // Extraction dynamique de la version de build (ex: "2024.3" -> "243", "2025.3" -> "253")
-        val buildNumber = platformVersion.substring(2, 4) + platformVersion.substring(5, 6)
-        sinceBuild.set(buildNumber)
+intellijPlatform {
+    pluginConfiguration {
+        id.set("org.labs.genesis")
+        name.set("Genesis")
+        
+        ideaVersion {
+            sinceBuild.set("251")
+            untilBuild.set("262.*")
+        }
     }
-
-    signPlugin {
+    
+    signing {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
         password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
     }
 
-    publishPlugin {
+    publishing {
         token.set(System.getenv("PUBLISH_TOKEN"))
     }
+}
 
-    buildPlugin {
-        archiveFileName.set("genesis-$artifactSuffix-${platformVersion}.zip")
-    }
-
+tasks {
     buildSearchableOptions {
         enabled = false
     }
