541121845b84d7bd88a26ecb2f66e94ef9767859
WesleyRS-creator <nyantemaranaivosolohery@gmail.com>
2026-08-29 18:31:55 +0300

    ajustements des thèmes colorimétriques

--- genesis-vsc/webview-ui/src/features/frameworks/components/FrameworkList.vue
+++ genesis-vsc/webview-ui/src/features/frameworks/components/FrameworkList.vue
@@ -11,7 +11,7 @@
             @click="$emit('select', framework, $event)"
             @info="$emit('info', framework)"
         >
-            <template #complementary>
+            <!-- <template #complementary>
                 <span
                     v-if="framework.isProd"
                     class="px-2 py-0.5 bg-green-500/10 text-green-500 text-xs rounded-full font-medium whitespace-nowrap"
@@ -21,6 +21,11 @@
                 <span class="px-2 py-0.5 bg-secondary/20 text-text text-xs rounded-full whitespace-nowrap">
                     {{ framework.type }}
                 </span>
+            </template> -->
+
+            <template #default>
+                <td class="p-3 text-center">{{ framework.name }}</td>
+                <td class="p-3 text-center">{{ framework.coreFramework }}</td>
             </template>
         </GenesisItem>
     </GenesisList>
